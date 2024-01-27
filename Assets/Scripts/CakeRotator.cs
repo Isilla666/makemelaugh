ab02f2dae481a77fc627b9a8fde53ca1ed4e3c24
isilla <neverovau5@gmail.com>
2024-01-28 03:14:08 +0500

    NEW sprites

--- Assets/Scripts/CakeRotator.cs
+++ Assets/Scripts/CakeRotator.cs
@@ -1,11 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CakeRotator : MonoBehaviour
 {
     [SerializeField] private float startRotatePos;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private List<Sprite> sprites;
+
+    private void Awake()
+    {
+        spriteRenderer.sprite = sprites[Random.Range(0, sprites.Count)];
+    }
 
     void Start()
     {
